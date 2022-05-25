6a432e45c5df05e479bdc04d0b98b6cc440b7ec6
riko-teki <boiledshrimps0109@gmail.com>
2022-05-25 12:02:40 +0900

    2022/5/25 AM

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
-use base64::{decode_config, encode_config};
+use base64::decode_config;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let get_token_url = "http://localhost:4567/token";
-    let submit_token_url = "http://localhost:4567/check";
+    let mut token_bytes = get_token().await?;
 
     let client = reqwest::Client::new();
-    let token_resp = client.get(get_token_url)
-        .send()
-        .await?;
-
-    let token: String = token_resp.text().await?;
-    let mut token_bytes: Vec<u8> = base64::decode_config(token.clone(), base64::URL_SAFE).unwrap() ;
-    
-    println!("{:?}", &token);
-
-    let submit_resp = client
-        .get(submit_token_url)
-        .query(&[("token",token.as_str())])
-        .send()
-        .await?;
-    
 
     // 最後の16バイトブロックを全て\x00にして送信する
     let zero_byte_count = 16;
@@ -28,19 +12,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for _ in 0..16 {
         token_bytes.push(0);
     }
-    
-    println!("{:?}", &token_bytes);
 
-    for i in 0..255 {
-        token_bytes.pop();
-        token_bytes.push(i);
-        let token_b64 = base64::encode_config(token_bytes.clone(), base64::URL_SAFE);
-        let submit_resp = reqwest::get(format!("http://localhost:4567/check?token={token_b64}"))
-        .await
-        .unwrap();
-
-        let body = submit_resp.text().await?;
-        println!("{}:{:?}", i,&body);
+    for i in 0..=255 {
+        //token_bytes.pop();
+        //token_bytes.push(i);
+        token_bytes[31] = i;
+        let result = submit_token(&client, token_bytes.clone()).await?;
+        if result != "decrypt error" {
+            println!("{}:{}:{:?}", token_bytes.len(), i, &result);
+        }
     }
     Ok(())
+}
+
+async fn get_token() -> Result<Vec<u8>, reqwest::Error> {
+    let get_token_url = "http://localhost:4567/token";
+
+    let token = reqwest::get(get_token_url).await?.text().await?;
+
+    // if base64 decode is failed, panic!
+    Ok(decode_config(token.clone(), base64::URL_SAFE).unwrap())
+}
+
+async fn submit_token(
+    client: &reqwest::Client,
+    token_bytes: Vec<u8>,
+) -> Result<String, reqwest::Error> {
+    let submit_token_url = "http://localhost:4567/check?token=";
+
+    let token_b64 = base64::encode_config(token_bytes.clone(), base64::URL_SAFE);
+    Ok(client
+        .get(format!("{}{}", submit_token_url, token_b64))
+        .send()
+        .await?
+        .text()
+        .await?)
 }
